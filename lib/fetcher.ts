cf754d2298ab38e656ea8469c4c86872f5261a2a
yoyoyojoe <jpma.family@gmail.com>
2022-10-18 16:06:51 -0700

    Log res.status

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,5 @@
+import { validateRoute } from './auth';
+
 export default function fetcher(url: string, data: any = undefined) {
   return fetch(`${window.location.origin}/api/${url}`, {
     method: data ? 'POST' : 'GET',
@@ -7,6 +9,9 @@ export default function fetcher(url: string, data: any = undefined) {
     },
     body: JSON.stringify(data),
   }).then((res) => {
+    if (process.env.NODE_ENV === 'development')
+      console.log('res status:', res.status);
+
     if (res.status > 399 || res.status < 200) throw new Error();
     return res.json();
   });
