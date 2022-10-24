1be153150b1e0576f2b4edab01ce1ddafb7de9a0
yoyoyojoe <jpma.family@gmail.com>
2022-10-24 15:40:45 -0700

    Implement PlayerBar component & play on click functionality

--- components/playerBar.tsx
+++ components/playerBar.tsx
@@ -1,19 +1,32 @@
 import { Box, Flex, Text } from '@chakra-ui/layout';
+import { useStoreState } from 'easy-peasy';
+import React from 'react';
+import Player from './player';
 
 export default function PlayerBar() {
+  const songs = useStoreState((state: any) => state.activeSongs);
+  const activeSong = useStoreState((state: any) => state.activeSong);
+
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="0.25em">
       <Flex align="center">
         <Box padding="0.25em 1em" color="white" width="30%">
-          <Text fontSize="large">song name</Text>
-          <Text fontSize="sm">artist name</Text>
+          {activeSong && (
+            <React.Fragment>
+              <Text fontSize="large">{activeSong.name}</Text>
+              <Text fontSize="sm">{activeSong.artist.name}</Text>
+            </React.Fragment>
+          )}
         </Box>
+
         <Box width="40%" display="flex" justifyContent="center">
-          Controls
+          {activeSong && (
+            <Player songs={songs} activeSong={activeSong}></Player>
+          )}
         </Box>
         <Box
+          color="gray.600"
           padding="0.25em 1em"
-          color="white"
           display="flex"
           justifyContent="flex-end"
           width="30%"
--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -2,18 +2,32 @@ import { Box } from '@chakra-ui/layout';
 import { Table, Thead, Td, Th, Tr, Tbody, IconButton } from '@chakra-ui/react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-import { formatDate, formatTime } from '../lib/formatter';
+import { formatTime } from '../lib/formatter';
+import { useStoreActions } from 'easy-peasy';
+
 const SongsTable = ({ songs }: any) => {
+  const setActiveSongs = useStoreActions(
+    (store: any) => store.changeActiveSongs
+  );
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+  function handlePlay(activeSong?: 'string') {
+    setActiveSong(activeSong || songs[0]);
+    setActiveSongs(songs);
+  }
+
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
         <Box marginLeft="1em" marginBottom="1.5em">
+          {/* Play Button */}
           <IconButton
             aria-label="press-play"
             icon={<BsFillPlayFill fontSize="30px" />}
             colorScheme="green"
             size="lg"
             isRound
+            onClick={() => handlePlay()}
           />
         </Box>
         <Table variant="unstyled">
@@ -35,6 +49,8 @@ const SongsTable = ({ songs }: any) => {
                   '&:hover': { bg: 'rgba(255,255,255,0.1)' },
                 }}
                 key={song.id}
+                cursor="pointer"
+                onClick={() => handlePlay(song)}
               >
                 <Td>{i + 1}</Td>
                 <Td>{song.name}</Td>
